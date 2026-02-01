af036d77a8cb924178e84db1999368d93004a214
heris-fsya <fsya45@gmail.com>
2026-02-01 15:12:10 +0100

    victoire implémenté mais pas dans la scene

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -106,6 +106,8 @@ public class LevelLoader : MonoBehaviour
         {
              UnityEngine.Debug.Log("Last level reached.");
             currentLevelIndex = levels.Count - 1;
+              audioManager.PlayVictoryMusic();
+        uiUpdater.ShowVictoryScreen();
             return;
         }
 
